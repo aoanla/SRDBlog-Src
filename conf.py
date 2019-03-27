932ac6c763c42fd7e9db09ba7e501e20cd01ca23
aoanla <aoanla@gmail.com>
2019-03-27 21:08:32 +0000

    www added to url

--- conf.py
+++ conf.py
@@ -20,7 +20,7 @@ BLOG_AUTHOR = "SRD"  # (translatable)
 BLOG_TITLE = "Scottish Roller Derby Blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://scottishrollerderbyblog.com/"
+SITE_URL = "https://www.scottishrollerderbyblog.com/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://scottishrollerderbyblog.com/"
