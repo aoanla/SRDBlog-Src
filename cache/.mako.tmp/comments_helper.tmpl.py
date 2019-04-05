f5c12b0a1c4f3f4c15cb41ef1210f82674a6dad8
aoanla <aoanla@gmail.com>
2019-04-05 21:59:57 +0100

    Road To Playoffs Apr 2019 publish

--- cache/.mako.tmp/comments_helper.tmpl.py
+++ cache/.mako.tmp/comments_helper.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554497807.379595
+_modified_time = 1554497971.362016
 _enable_loop = True
 _template_filename = '/Users/samskipsey/Documents/Project/SRD-archive/scottishrollerderby.wordpress.com-2019-01-11-14_23_06/nikola/lib/python3.7/site-packages/nikola/data/themes/base/templates/comments_helper.tmpl'
 _template_uri = 'comments_helper.tmpl'
@@ -62,12 +62,12 @@ def render_comment_form(context,url,title,identifier):
     __M_caller = context.caller_stack._push_frame()
     try:
         comment_system = context.get('comment_system', UNDEFINED)
-        disqus = _mako_get_namespace(context, 'disqus')
-        muut = _mako_get_namespace(context, 'muut')
-        isso = _mako_get_namespace(context, 'isso')
-        facebook = _mako_get_namespace(context, 'facebook')
-        commento = _mako_get_namespace(context, 'commento')
         intensedebate = _mako_get_namespace(context, 'intensedebate')
+        facebook = _mako_get_namespace(context, 'facebook')
+        isso = _mako_get_namespace(context, 'isso')
+        commento = _mako_get_namespace(context, 'commento')
+        muut = _mako_get_namespace(context, 'muut')
+        disqus = _mako_get_namespace(context, 'disqus')
         __M_writer = context.writer()
         __M_writer('\n')
         if comment_system == 'disqus':
@@ -103,12 +103,12 @@ def render_comment_link(context,link,identifier):
     __M_caller = context.caller_stack._push_frame()
     try:
         comment_system = context.get('comment_system', UNDEFINED)
-        disqus = _mako_get_namespace(context, 'disqus')
-        muut = _mako_get_namespace(context, 'muut')
-        isso = _mako_get_namespace(context, 'isso')
-        facebook = _mako_get_namespace(context, 'facebook')
-        commento = _mako_get_namespace(context, 'commento')
         intensedebate = _mako_get_namespace(context, 'intensedebate')
+        facebook = _mako_get_namespace(context, 'facebook')
+        isso = _mako_get_namespace(context, 'isso')
+        commento = _mako_get_namespace(context, 'commento')
+        muut = _mako_get_namespace(context, 'muut')
+        disqus = _mako_get_namespace(context, 'disqus')
         __M_writer = context.writer()
         __M_writer('\n')
         if comment_system == 'disqus':
@@ -144,12 +144,12 @@ def render_comment_link_script(context):
     __M_caller = context.caller_stack._push_frame()
     try:
         comment_system = context.get('comment_system', UNDEFINED)
-        disqus = _mako_get_namespace(context, 'disqus')
-        muut = _mako_get_namespace(context, 'muut')
-        isso = _mako_get_namespace(context, 'isso')
-        facebook = _mako_get_namespace(context, 'facebook')
-        commento = _mako_get_namespace(context, 'commento')
         intensedebate = _mako_get_namespace(context, 'intensedebate')
+        facebook = _mako_get_namespace(context, 'facebook')
+        isso = _mako_get_namespace(context, 'isso')
+        commento = _mako_get_namespace(context, 'commento')
+        muut = _mako_get_namespace(context, 'muut')
+        disqus = _mako_get_namespace(context, 'disqus')
         __M_writer = context.writer()
         __M_writer('\n')
         if comment_system == 'disqus':
--- cache/.mako.tmp/index.tmpl.py
+++ cache/.mako.tmp/index.tmpl.py
@@ -5,7 +5,7 @@ STOP_RENDERING = runtime.STOP_RENDERING
 __M_dict_builtin = dict
 __M_locals_builtin = locals
 _magic_number = 10
-_modified_time = 1554497808.675174
+_modified_time = 1554497971.3385558
 _enable_loop = True
 _template_filename = 'themes/monospace/templates/index.tmpl'
 _template_uri = 'index.tmpl'
@@ -36,17 +36,17 @@ def render_body(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
         __M_locals = __M_dict_builtin(pageargs=pageargs)
-        messages = context.get('messages', UNDEFINED)
-        comments = _mako_get_namespace(context, 'comments')
-        helper = _mako_get_namespace(context, 'helper')
-        posts = context.get('posts', UNDEFINED)
-        date_format = context.get('date_format', UNDEFINED)
-        math = _mako_get_namespace(context, 'math')
         index_teasers = context.get('index_teasers', UNDEFINED)
-        def extra_head():
-            return render_extra_head(context._locals(__M_locals))
+        posts = context.get('posts', UNDEFINED)
         def content():
             return render_content(context._locals(__M_locals))
+        comments = _mako_get_namespace(context, 'comments')
+        math = _mako_get_namespace(context, 'math')
+        messages = context.get('messages', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        date_format = context.get('date_format', UNDEFINED)
+        def extra_head():
+            return render_extra_head(context._locals(__M_locals))
         _link = context.get('_link', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
@@ -71,10 +71,10 @@ def render_body(context,**pageargs):
 def render_extra_head(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
+        posts = context.get('posts', UNDEFINED)
+        math = _mako_get_namespace(context, 'math')
         def extra_head():
             return render_extra_head(context)
-        math = _mako_get_namespace(context, 'math')
-        posts = context.get('posts', UNDEFINED)
         __M_writer = context.writer()
         __M_writer('\n')
         __M_writer(str(math.math_styles_ifposts(posts)))
@@ -87,13 +87,13 @@ def render_extra_head(context,**pageargs):
 def render_content(context,**pageargs):
     __M_caller = context.caller_stack._push_frame()
     try:
-        messages = context.get('messages', UNDEFINED)
-        comments = _mako_get_namespace(context, 'comments')
-        helper = _mako_get_namespace(context, 'helper')
-        posts = context.get('posts', UNDEFINED)
-        date_format = context.get('date_format', UNDEFINED)
-        math = _mako_get_namespace(context, 'math')
         index_teasers = context.get('index_teasers', UNDEFINED)
+        posts = context.get('posts', UNDEFINED)
+        comments = _mako_get_namespace(context, 'comments')
+        math = _mako_get_namespace(context, 'math')
+        messages = context.get('messages', UNDEFINED)
+        helper = _mako_get_namespace(context, 'helper')
+        date_format = context.get('date_format', UNDEFINED)
         def content():
             return render_content(context)
         _link = context.get('_link', UNDEFINED)
